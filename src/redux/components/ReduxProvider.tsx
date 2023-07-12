a1c1455cfdf1979c97cdee8392a2e6335ebf8579
sohodoll <kovalevvtl@gmail.com>
2023-07-12 16:32:00 +0300

    feat: add new comment

--- src/redux/components/ReduxProvider.tsx
+++ src/redux/components/ReduxProvider.tsx
@@ -5,6 +5,8 @@ import { store } from '../store'
 import { Provider } from 'react-redux'
 import { ThemeProvider } from 'next-themes'
 
+//the core component that wraps the entire app with the redux store and the next-themes provider
+
 export const ReduxProvider = ({ children }: { children: ReactNode }) => {
   return (
     <Provider store={store}>
